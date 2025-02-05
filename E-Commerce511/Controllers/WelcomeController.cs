b879717c4fa03eecd4ca12aed2679f636d3d0946
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-02-05 20:57:15 +0200

    First Controller & View

---
+++ E-Commerce511/Controllers/WelcomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce511.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            string name = "Mohamed";
+            int age = 25;
+            List<string> skills = new List<string>();
+            skills.Add("C++");
+            skills.Add("C#");
+            skills.Add("SQL");
+
+            return View(skills);
+        }
+    }
+}
