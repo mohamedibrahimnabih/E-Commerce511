68cb73f287d284576f2b2115f7b2870e8086c7b1
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-02-09 20:44:11 +0200

    Details Page

--- E-Commerce511/Controllers/HomeController.cs
+++ E-Commerce511/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace E_Commerce511.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        ApplicationDbContext dbContext = new ApplicationDbContext();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -16,6 +17,39 @@ namespace E_Commerce511.Controllers
         }
 
         public IActionResult Index()
+        {
+            var products = dbContext.Products.Include(e=>e.Category);
+            //
+            return View(products.ToList());
+        }
+
+        public IActionResult Details(int productId)
+        {
+            var product = dbContext.Products.Include(e => e.Category).FirstOrDefault(e => e.Id == productId);
+
+            if(product != null)
+            {
+                var relatedProducts = dbContext.Products.Include(e => e.Category).Where(e => e.CategoryId == product.CategoryId).Skip(0).Take(4).ToList();
+
+                //ProductsWithCategory productsWithCategory = new ProductsWithCategory()
+                //{
+                //    Product = product,
+                //    ProductsWithCategories = relatedProducts
+                //};
+
+                var productsWithCategory = new
+                {
+                    Product = product,
+                    ProductsWithCategories = relatedProducts
+                };
+
+                return View(productsWithCategory);
+            }
+
+            return RedirectToAction(nameof(NotFoundPage));
+        }
+
+        public IActionResult NotFoundPage()
         {
             return View();
         }
---
+++ E-Commerce511/Models/ViewModels/ProductsWithCategory.cs
@@ -0,0 +1,8 @@
+﻿//namespace E_Commerce511.Models.ViewModels
+//{
+//    public class ProductsWithCategory
+//    {
+//        public Product Product { get; set; }
+//        public List<Product> ProductsWithCategories { get; set; }
+//    }
+//}
