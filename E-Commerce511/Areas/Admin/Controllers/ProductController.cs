f3aa0b0442da05f344f419ad91e84fcc75ab56ed
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-02-16 21:52:40 +0200

    CR Operations on Product Model

---
+++ E-Commerce511/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using E_Commerce511.DataAccess;
+using E_Commerce511.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce511.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            var products = dbContext.Products.Include(e => e.Category);
+
+            return View(products.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var categories = dbContext.Categories;
+            //ViewBag.Categories = categories;
+            ViewData["Categories"] = categories.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product, IFormFile file)
+        {
+            // Validation
+
+            if (file != null && file.Length > 0)
+            {
+                // Save img in wwwroot
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(stream);
+                }
+
+                product.Img = fileName;
+            }
+
+            if (product != null)
+            {
+                dbContext.Products.Add(product);
+                dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+    }
+}
--- E-Commerce511/Models/Product.cs
+++ E-Commerce511/Models/Product.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Img { get; set; }
+        public string? Img { get; set; }
         public double Rate { get; set; }
         public int Quantity { get; set; }
         public int CategoryId { get; set; }
